5413a728347d20837fc12fe93842265002e8126b
Danila Ridzhi <danilaridzhi@gmail.com>
2015-12-15 15:10:42 +0300

    add Argument helper

---
+++ src/Argument.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace DocBuilder;
+
+
+class Argument
+{
+
+    const DEFAULT_NOT_ENABLED = '___null___';
+
+    protected $name;
+    protected $type;
+    protected $default;
+
+    /**
+     * @param string $expression
+     */
+    public function __construct($expression = null)
+    {
+        if ($expression !== null) {
+            $info = $this->parse($expression);
+            $this->name = $info['name'];
+            $this->type = $info['type'];
+            $this->default = $info['default'];
+        }
+    }
+
+    /**
+     * @param string $type
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+    }
+
+    /**
+     * @param string $default
+     */
+    public function setDefault($default)
+    {
+        $this->default = $default;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDefault()
+    {
+        return $this->default;
+    }
+
+    /**
+     * @param string $expression
+     * @return array
+     * @throws \InvalidArgumentException
+     */
+    protected function parse($expression)
+    {
+        preg_match('/(\w+(?=\:))?\:?([\w0-9_]+)\=?((?<=\=).*)?/', $expression, $matches);
+
+        $count = count($matches);
+
+        if ($count < 3) {
+            throw new \InvalidArgumentException('Not valid expression to parse');
+        }
+
+        if ($count === 3) {
+            $matches[3] = Argument::DEFAULT_NOT_ENABLED;
+        }
+
+        return [
+            'type' => $matches[1],
+            'name' => $matches[2],
+            'default' => $matches[3]
+        ];
+
+    }
+
+}
---
+++ tests/ArgumentTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace DocBuilder;
+
+
+class ArgumentTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @dataProvider parseProvider
+     */
+    public function testParse($expression, $type, $name, $default)
+    {
+        $arg = new Argument();
+        $ref = new \ReflectionClass($arg);
+        $method = $ref->getMethod('parse');
+        $method->setAccessible(true);
+        $actual = $method->invoke($arg, $expression);
+        $expected = [
+            'type' => $type,
+            'name' => $name,
+            'default' => $default
+        ];
+        $this->assertEquals($expected, $actual);
+    }
+
+
+    public function parseProvider()
+    {
+        return [
+            ['id', '', 'id', Argument::DEFAULT_NOT_ENABLED],
+            ['int:id', 'int', 'id', Argument::DEFAULT_NOT_ENABLED],
+            ['int:id=404', 'int', 'id', 404]
+        ];
+    }
+
+}
